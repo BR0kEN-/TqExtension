3706a824e45e32127506396b92ee0cb41525db8b
Sergii Bondarenko <broken@propeople.com.ua>
2016-04-01 15:05:39 +0300

    Add environment loader Behat library

--- src/ServiceContainer/TqExtension.php
+++ src/ServiceContainer/TqExtension.php
@@ -4,39 +4,19 @@
  */
 namespace Drupal\TqExtension\ServiceContainer;
 
-use Behat\Behat\Context\ServiceContainer\ContextExtension;
+use Behat\EnvironmentLoader;
 use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
-use Behat\Testwork\ServiceContainer\ServiceProcessor;
-use Behat\Testwork\Environment\ServiceContainer\EnvironmentExtension;
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
+/**
+ * Class TqExtension.
+ *
+ * @package Drupal\TqExtension\ServiceContainer
+ */
 class TqExtension implements Extension
 {
-    /**
-     * @var ServiceProcessor
-     */
-    private $processor;
-    private $baseNamespace = '';
-
-    /**
-     * Initializes compiler pass.
-     *
-     * @param null|ServiceProcessor $processor
-     */
-    public function __construct(ServiceProcessor $processor = null)
-    {
-        $this->processor = $processor ?: new ServiceProcessor;
-        $this->baseNamespace = implode('\\', array_slice(explode('\\', __NAMESPACE__), 0, -1));
-    }
-
-    public function contextNamespace($suffix = '')
-    {
-        return "$this->baseNamespace\\Context" . ($suffix ? "\\$suffix" : '');
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -57,11 +37,8 @@ class TqExtension implements Extension
      */
     public function load(ContainerBuilder $container, array $config)
     {
-        $config['context_namespace'] = $this->contextNamespace();
-
-        $this->setDefinition($container, 'TqContextInitializer', ContextExtension::INITIALIZER_TAG, [
-            $config,
-        ]);
+        $loader = new EnvironmentLoader($this, $container, $config);
+        $loader->load();
     }
 
     /**
@@ -71,9 +48,6 @@ class TqExtension implements Extension
      */
     public function process(ContainerBuilder $container)
     {
-        $this->setDefinition($container, 'TqContextReader', EnvironmentExtension::READER_TAG . '.context', [
-            $this->processor->findAndSortTaggedServices($container, ContextExtension::READER_TAG),
-        ]);
     }
 
     /**
@@ -137,24 +111,5 @@ class TqExtension implements Extension
         }
 
         $config->end()->end()->end()->end();
-    }
-
-    /**
-     * Add definition to DI container.
-     *
-     * @param ContainerBuilder $container
-     *   DI container.
-     * @param string $class
-     *   Class name (in TqExtension namespace).
-     * @param string $id
-     *   Definition ID.
-     * @param array $arguments
-     *   Definition arguments.
-     */
-    private function setDefinition(ContainerBuilder $container, $class, $id, array $arguments = [])
-    {
-        $definition = new Definition($this->contextNamespace($class), $arguments);
-        $definition->addTag($id, ['priority' => 0]);
-        $container->setDefinition($id, $definition);
     }
 }
